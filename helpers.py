17e71285388f2d59fa319d93ed3525c59f82a37f
bozkurta19 <58877974+bozkurta19@users.noreply.github.com>
2022-12-02 00:28:40 +0300

    Add createSubmission and testScore functions, fix and adjust the code

--- helpers.py
+++ helpers.py
@@ -1,7 +1,10 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error as mse
 import numpy as np
+import pandas as pd
+
 
 def labelEncode(data):
     le = LabelEncoder()
@@ -11,7 +14,25 @@ def labelEncode(data):
     ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [5])], remainder='passthrough')
     data = np.array(ct.fit_transform(data))
 
-    data = data
-
     return data
 
+
+def createSubmission(regressor):
+    test_features = pd.read_csv("test_features.csv")
+    test_features = labelEncode(test_features)
+
+    with open('submission.csv', 'w') as outFile:
+        predictions = regressor.predict(test_features)
+        outFile.write("ID,predicted\n")
+        for i in range(len(predictions)):
+            outFile.write(str(i) + ',' + str(predictions[i]) + "\n")
+
+
+def testScore(regressor):
+    test_features = pd.read_csv("test_features.csv")
+    test_features = labelEncode(test_features)
+
+    test_targets = pd.read_csv("test_targets.csv")
+    test_targets = test_targets.values
+
+    return mse(test_targets, regressor.predict(test_features))
--- main.py
+++ main.py
@@ -1,59 +1,52 @@
-import pandas as pd
-import random as r
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
+from copy import deepcopy
+import pandas as pd
+import random as r
 import helpers
 
+if __name__ == "__main__":
+    r.seed(1)
 
-train_features = pd.read_csv("train_features.csv")
-train_targets = pd.read_csv("train_targets.csv")
-test_features = pd.read_csv("test_features.csv")
+    train_features = pd.read_csv("train_features.csv")
+    train_targets = pd.read_csv("train_targets.csv")
+    test_features = pd.read_csv("test_features.csv")
 
-train_features = helpers.labelEncode(train_features)
-test_features = helpers.labelEncode(test_features)
-train_targets = train_targets.values
+    train_features = helpers.labelEncode(train_features)
+    test_features = helpers.labelEncode(test_features)
+    train_targets = train_targets.values
 
+    kf = KFold(n_splits=5)
+    best_regressor = None
+    best_regressor_error = 999999999
+    regressor = LinearRegression() # For loop
+    i = 1
 
-regressor = LinearRegression()
+    for train, test in kf.split(train_features):
+        x_train = train_features[train]
+        x_test = train_features[test]
 
-r.seed(1)
-kf = KFold(n_splits=5, shuffle=True)
-i = 1
+        y_train = train_targets[train].flatten()
+        y_test = train_targets[test].flatten()
 
-for train, test in kf.split(train_features):
-    x_train = train_features[train]
-    x_test = train_features[test]
+        regressor.fit(x_train, y_train)
 
-    y_train = train_targets[train].flatten()
-    y_test = train_targets[test].flatten()
+        print("{}. Fold : ".format(i))
+        print("Train error : {}".format(mse(y_train, regressor.predict(x_train))))
+        print("Train score : {}".format(regressor.score(x_train, y_train)))
+        print()
 
-    regressor.fit(x_train, y_train)
+        regressor_error = mse(y_test, regressor.predict(x_test))
+        print("Test  error : {}".format(regressor_error))
+        print("Test  score : {}".format(regressor.score(x_test, y_test)))
+        print("------------------------------------------------")
 
-    print("{}. Fold : ".format(i))
-    print("Train error : {}".format(mse(y_train, regressor.predict(x_train))))
-    print("Train regressor Score : ", regressor.score(x_train, y_train))
-    print()
-    print("Test  error : {}".format(mse(y_test, regressor.predict(x_test))))
-    print("Test  regressor Score : ", regressor.score(x_test, y_test))
-    print("------------------------------------------------")
+        if regressor_error < best_regressor_error:
+            best_regressor = deepcopy(regressor) # If we don't deepcopy, best_regressor gets updated everytime regressor is updated
+            best_regressor_error = regressor_error
 
-    i += 1
+        i += 1
 
-
-"""
-label = LabelEncoder()
-label.fit(train_features.sex.drop_duplicates())
-train_features.sex = label.transform(train_features.sex)
-label.fit(train_features.smoker.drop_duplicates())
-train_features.smoker = label.transform(train_features.smoker)
-label.fit(train_features.region.drop_duplicates())
-train_features.region = label.transform(train_features.region)
-train_features.dtypes
-
-lr = LinearRegression().fit(train_features, train_targets)
-
-train_predictions = lr.predict(train_features)
-
-print(lr.score(train,y_test))
-"""
+    print("Score on test data : {}".format(helpers.testScore(best_regressor)))
+    helpers.createSubmission(best_regressor)
